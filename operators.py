73ee463caa4e29083c06f291fcdd06e2814d932b
emmh707 <pretty1emmaculate@gmail.com>
2025-02-04 16:45:43 +0300

    first commit

---
+++ operators.py
@@ -0,0 +1,61 @@
+#arithmetic operators(+,-,/,%,*)
+# a = 23
+# b = 14
+# total = a + b
+# print( "The total is", total)
+# subtract = a - b
+# print(f'The subtract is {subtract}')
+# remainder = a % b
+# print("The remainder is",remainder)
+# #comparison operators(==,>,<,>=,<=,!=)
+# age1 = 23
+# age2 = 12
+# print(f'Is age1 equal age2?  {age1 == age2}')
+# print(f'Is age greater than age2?  {age1 > age2}')
+# print(f'Is age1 less than age2?  {age1 < age2}')
+# print(f'Is age1 greater than or equal to age2 {age1>=age2}')
+# print(f'Is age1 less than or equal to age2 {age1<=age2}')
+# print(f'Is age1 not equal to age2 {age1!=age2}')
+# #logical operators(and,or,not)
+# math = 67
+# science = 56
+# swahili = 78
+# french = 59
+# math > science and swahili > french
+# print(math>science and swahili>french)
+# print(math<science and swahili<french)
+# print(math>=science and swahili<=french)
+# print(math>science or swahili>french)
+# print(math<science or  swahili<french)
+# print(not(math>=science or swahili<=french))
+
+# name = input("What is your name? ")
+# print(name)
+# First_number = float(input("What is your first number? "))
+# print(First_number)
+# Second_number = float(input("What is your second number? "))
+# print(Second_number)
+# Third_number = float(input("What is your third number? "))
+# print(Third_number)
+# Fourth_number = float(input("What is your fourth number? "))
+# print(Fourth_number)
+# divide1 = First_number / Second_number
+# print(f'divide1 is {divide1}')
+# divide2 = Second_number / Third_number
+# print(f' divide2 is {divide2}')
+# print(f'Is divide1 less than divide2?  {divide1 < divide2}')
+# print(f'Is divide1 equal divide2?  {divide1 == divide2}')
+# print(f'Is divide1 greater than divide2?  {divide1 > divide2}')
+# print(f'Is divide1 not equal to divide2?  {divide1 != divide2}')
+# print(f'both results are positive {divide1>0 and divide2>0}')
+# print(f'both results are negative {divide1<0 and divide2<0}')
+# print(f'at least one result is zero {divide1==0 or  divide2==0}')
+#
+
+name = input("What is your name? ")
+print(name)
+username = input("What is your username? ")
+print(username)
+password = input("What is your password? ")
+print(password)
+print(f'both username and password are correct {username=="admin" and password=="secure123"}')
---
+++ variables.py
@@ -0,0 +1,50 @@
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print(124557)
+print(777.59)
+# variables
+first_name = "Immaculate"
+print(first_name)
+age = 18
+print(age)
+weight = 48.5
+print(weight)
+
+# data types
+# integer
+a = 1341
+print(a)
+# string
+b = "Click to save the form"
+print(b)
+# float
+d = 2364.8637654
+print(d)
+# boolean
+is_on = True
+is_off = False
+
+#input in user
+name = input("What is your name? ")
+print(name)
+# age = input("How old are you? ")
+# print(age)
+# weight = input("How much do you weigh? ")
+# print(weight)
+# first_number = float(input("What is your first lucky number? "))
+# second_number = float(input("What is your second lucky number? "))
+# total = first_number + second_number
+# print(total)
+ # take in the users height and weight and calculate the BMI then display the BMI
+weight = float(input("What is your weight? "))
+height = float(input("What is your height? "))
+BMI = weight / (height * height)
+print(BMI)
+full_name = input("What is your full name? ")
+age = float(input("How old are you? "))
+print(age)
+year_of_birth = float(input("What is your year of birth? "))
+print(year_of_birth)
+year_of_turning_100 = 100 + year_of_birth
+print(year_of_turning_100)
